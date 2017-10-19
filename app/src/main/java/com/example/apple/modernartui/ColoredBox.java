7969477e67efd88d42259d282707641dc4f75cde
ShawnNew <ncxinhanzhong@gmail.com>
2017-10-19 17:05:32 -0400

    Add the function of the seekbar

--- app/src/main/java/com/example/apple/modernartui/ColoredBox.java
+++ app/src/main/java/com/example/apple/modernartui/ColoredBox.java
@@ -2,7 +2,9 @@ package com.example.apple.modernartui;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.net.sip.SipAudioCall;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,15 +13,62 @@ import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.DialogInterface;
+import android.widget.GridView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 
 public class ColoredBox extends AppCompatActivity {
+    SeekBar colorSeekBar;
+    GridView gridView1;
+    GridView gridView2;
+    GridView gridView3;
+    GridView gridView4;
+    GridView gridView5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colored_box);
+
+        // Set the view form the main resource
+        colorSeekBar = (SeekBar) findViewById(R.id.SlideBar);
+        gridView1 = (GridView) findViewById(R.id.gridView1);
+        gridView2 = (GridView) findViewById(R.id.gridView2);
+        gridView3 = (GridView) findViewById(R.id.gridView3);
+        gridView4 = (GridView) findViewById(R.id.gridView4);
+        gridView5 = (GridView) findViewById(R.id.gridView5);
+
+        // Assign this class as a listener for the SeekBar events
+        SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                gridView1.setBackgroundColor(Color.rgb(107 + (148 / 100) * progress
+                        , 120 + (134 / 100) * progress
+                        , 181 + (6 / 100) * progress));
+                gridView2.setBackgroundColor(Color.rgb(212 + (42 / 100) * progress
+                        , 82 + (146 / 100) * progress
+                        , 150 + (6 / 100) * progress));
+                gridView3.setBackgroundColor((Color.rgb(161 + (92 / 100) * progress
+                        , 31 + (147 / 100) * progress
+                        , 38 + (17 / 100) * progress)));
+                gridView5.setBackgroundColor(Color.rgb(41 + (148 / 100) * progress
+                        , 61 + (146 / 100) * progress
+                        , 149 + (4 / 100) * progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+        colorSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
     }
 
 
@@ -29,8 +78,6 @@ public class ColoredBox extends AppCompatActivity {
 
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.my_option_menu, menu);
-        //MenuItem moreInfo = menu.findItem(R.id.more);
-        //moreInfo.setEnabled(true);
         return super.onCreateOptionsMenu(menu);
         //return true;
     }
